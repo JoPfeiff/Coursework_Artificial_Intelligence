f07ca779a717400b589ea50c7326416d5f7278f3
Aishwarya Kamath <aishwaryakamath@Aishwaryas-MacBook-Pro.local>
2017-02-09 22:32:25 -0500

    plots

--- HW1/q5.py
+++ HW1/q5.py
@@ -5,6 +5,7 @@ from operator import itemgetter
 from collections import defaultdict
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 class Node():
 
@@ -97,7 +98,8 @@ def a_star(x1,y1,x2,y2):
     start.set_cost(0)
 
     count=0
-
+    
+    t = time.time()
     while bool(stack):
         stack_item = stack.pop_node()
         currenct_h_cost = stack_item[1]
@@ -120,8 +122,8 @@ def a_star(x1,y1,x2,y2):
                 child.set_cost(new_cost )
                 child.set_parent(current_node)
                 stack.add_node(child, new_cost + heuristic(child, goal))
-
-
+    exec_time=time.time() - t
+    print 'Time to generate the route (seconds): ', exec_time
     print("Path :")
     parent = goal.get_parent()
     print(goal.get_node_name())
@@ -132,7 +134,7 @@ def a_star(x1,y1,x2,y2):
 
     print("cost = %s" %(goal.get_cost()))
     print count
-    return count, goal.get_cost() 
+    return count, goal.get_cost(), exec_time
 
 
 def add_edge(graph, vertex_a, vertex_b):
@@ -166,16 +168,22 @@ def legal_moves_from(row, col, board_size):
 
 counts=[]
 costs=[]
+times=[]
 for i in range(100):
-    x,y=a_star(np.random.randint(0,8),np.random.randint(0,8),np.random.randint(0,8),np.random.randint(0,8))
+    x,y,z=a_star(np.random.randint(0,8),np.random.randint(0,8),np.random.randint(0,8),np.random.randint(0,8))
     counts.append(x)
     costs.append(y)
-print len(counts)
-print len(costs)
+    times.append(z)
+#print len(counts)
+#print len(costs)
 
 for i in range(100):
     plt.scatter(costs[i],counts[i])
 plt.show()
 
+for i in range(100):
+    plt.scatter(costs[i],times[i])
+plt.show()
+    
 
 
